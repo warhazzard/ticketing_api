a3c0fe1ce7fd8ff1caca2c4d4f6c1bab2a521672
warhazzard <peeyush_j@ce.iitr.ac.in>
2026-08-01 19:37:12 +0530

    feat: add models and initialize database tables

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+
+from app.database import engine, Base 
+from app.models import User, Event, Tickets 
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield  
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    title="Ticketing API"
+)
+
+
+@app.get("/", include_in_schema=False)
+async def health_check():
+    return {"status": "System Online and Connected to PostgreSQL"}
+
+
+@app.get("/scalar", include_in_schema=False)
+async def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Ticketing API"
+    )
